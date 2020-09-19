611cd2924bf3c2b6b518907dac1aa3e7f4b4a039
BalaRama Krishna Oguri <balaramakrishna.oguri@gmail.com>
2020-09-19 17:41:46 +0530

    Add the ability to download more than one videos. Create a set of youtube links and iterate over it for each link to download the videos one by one.

--- run_this.py
+++ run_this.py
@@ -3,45 +3,57 @@ from mhmovie.code import *
 
 temp_folder = "temp_folder"
 # prompt to ask for the video link
-video_url = input("Paste the link of the video: ")
 
-# video format to be downloaded according to youtube-dl
-video_format = "399"  # FHD format
-# video_format = "394"  # lowest quality format
-audio_format = "140"
+# video_url = input("Paste the link of the video: ")
+youtube_link_list = []
+condition = True
+while condition:
+    link = input("Paste the link of the video: ")
+    if link == "":
+        condition = False
+        continue
+    youtube_link_list.append(link)
 
-# make a temporary directory named 'temp_folder'
-os.mkdir('.\\{}'.format(temp_folder))
-# change working directory to 'cache'
-os.chdir('.\\{}'.format(temp_folder))
+# Check for repeating links
+final_youtube_link_list = set(youtube_link_list)
+for video_url in final_youtube_link_list:
+    # video format to be downloaded according to youtube-dl
+    video_format = "399"  # FHD format
+    # video_format = "394"  # lowest quality format
+    audio_format = "140"
 
-# cmd command to download the video and audio of the specified formats.
-os.system('cmd /c "youtube-dl -f {} {}"'.format(video_format, video_url))
-os.system('cmd /c "youtube-dl -f {} {}"'.format(audio_format, video_url))
+    # make a temporary directory named 'temp_folder'
+    os.mkdir('.\\{}'.format(temp_folder))
+    # change working directory to 'cache'
+    os.chdir('.\\{}'.format(temp_folder))
 
-# change the format of the audio file from .m4a to .mp3
-# delete the .m4a file after conversion
-final_file_name = ""
-for file in os.listdir():
-    if '.mp4' in file:
-        final_file_name = file
+    # cmd command to download the video and audio of the specified formats.
+    os.system('cmd /c "youtube-dl -f {} {}"'.format(video_format, video_url))
+    os.system('cmd /c "youtube-dl -f {} {}"'.format(audio_format, video_url))
 
-    if '.m4a' in file:
-        os.system('cmd /c "ffmpeg -i \"{}\" audio.mp3"'.format(file))
-        os.remove(file)
+    # change the format of the audio file from .m4a to .mp3
+    # delete the .m4a file after conversion
+    final_file_name = ""
+    for file in os.listdir():
+        if '.mp4' in file:
+            final_file_name = file
 
-os.chdir('..')
+        if '.m4a' in file:
+            os.system('cmd /c "ffmpeg -i \"{}\" audio.mp3"'.format(file))
+            os.remove(file)
 
-# using mhmovie library, combining video with audio.
-f = folder("{}".format(temp_folder))
-f.save()
+    os.chdir('..')
 
-# renaming the output video file and moving it to the primary folder
-os.rename('.\\{}\\output.mp4'.format(temp_folder), '.\\' + final_file_name)
+    # using mhmovie library, combining video with audio.
+    f = folder("{}".format(temp_folder))
+    f.save()
 
-# deleting all the files in the cache folder
-for file in os.listdir('{}'.format(temp_folder)):
-    os.remove(os.path.join(temp_folder, file))
+    # renaming the output video file and moving it to the primary folder
+    os.rename('.\\{}\\output.mp4'.format(temp_folder), '.\\' + final_file_name)
 
-# finally removing the cache folder
-os.rmdir(temp_folder)
+    # deleting all the files in the cache folder
+    for file in os.listdir('{}'.format(temp_folder)):
+        os.remove(os.path.join(temp_folder, file))
+
+    # finally removing the cache folder
+    os.rmdir(temp_folder)
