433e9ddc7e315bf5760dc13b6acfc368ae0818f2
BalaRama Krishna Oguri <balaramakrishna.oguri@gmail.com>
2020-09-19 16:42:30 +0530

    Rename output video with original filename Remove prompt for a new name. Now, the newly generated file will be named the same as the file name of the original video file.

--- run_this.py
+++ run_this.py
@@ -1,8 +1,8 @@
 import os
 from mhmovie.code import *
 
-temp_folder = "temp folder"
-# promt to ask for the video link
+temp_folder = "temp_folder"
+# prompt to ask for the video link
 video_url = input("Paste the link of the video: ")
 
 # video format to be downloaded according to youtube-dl
@@ -10,7 +10,7 @@ video_format = "399"  # FHD format
 # video_format = "394"  # lowest quality format
 audio_format = "140"
 
-# make a temporary directory named 'cache'
+# make a temporary directory named 'temp_folder'
 os.mkdir('.\\{}'.format(temp_folder))
 # change working directory to 'cache'
 os.chdir('.\\{}'.format(temp_folder))
@@ -21,7 +21,11 @@ os.system('cmd /c "youtube-dl -f {} {}"'.format(audio_format, video_url))
 
 # change the format of the audio file from .m4a to .mp3
 # delete the .m4a file after conversion
+final_file_name = ""
 for file in os.listdir():
+    if '.mp4' in file:
+        final_file_name = file
+
     if '.m4a' in file:
         os.system('cmd /c "ffmpeg -i \"{}\" audio.mp3"'.format(file))
         os.remove(file)
@@ -33,8 +37,7 @@ f = folder("{}".format(temp_folder))
 f.save()
 
 # renaming the output video file and moving it to the primary folder
-new_name = input('Please enter a new name for the newly generated video file: ')
-os.rename('.\\{}\\output.mp4'.format(temp_folder), '.\\' + new_name + '.mp4')
+os.rename('.\\{}\\output.mp4'.format(temp_folder), '.\\' + final_file_name)
 
 # deleting all the files in the cache folder
 for file in os.listdir('{}'.format(temp_folder)):
