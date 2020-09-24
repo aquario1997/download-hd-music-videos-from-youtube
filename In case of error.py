a1731d2592766a1bb1f11f61c61d9a1603d9e974
BalaRama Krishna Oguri <balaramakrishna.oguri@gmail.com>
2020-09-24 18:58:44 +0530

    Add a new .py file to use in case of error with run_this.py file This new file is to be used in case of any error while using run_this.py file. This file includes a script which combines both the .mp4 and .mp3 files in the temporary file.

---
+++ In case of error.py
@@ -0,0 +1,6 @@
+from mhmovie.code import *
+
+f = folder("temp_folder")
+# some_folder has file of type mp4 and file of type mp3 and joins them automatically
+
+f.save("the_final.mp4")  #save movie+music as the final.mp4 in the folder
